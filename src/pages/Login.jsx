3eba6c4e158abd6af567e36e8bfeb154be66b67c
NPL Codes <104311071+amin-leon@users.noreply.github.com>
2024-05-28 21:44:57 +0200

    fixing ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -97,7 +97,7 @@ const LoginForm = () => {
           <div className="flex justify-center self-center z-10">
             <div className="p-12 bg-white mx-auto rounded-2xl w-100">
               <div className="mb-4">
-                <h3 className="font-semibold text-2xl text-center text-gray-800 pb-10">Welcome again</h3>
+                <h3 className="font-semibold text-2xl text-center text-gray-800 pb-10">Welcome Back</h3>
                 <p className="text-gray-500">Please sign in to your account.</p>
               </div>
               <div className="space-y-5">
